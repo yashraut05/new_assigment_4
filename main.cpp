7ff6be292007f6a82c6daaf6a5aebb96ae2c59f2
Yash Raut 05 <131888738+yashraut05@users.noreply.github.com>
2023-05-11 20:49:44 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int radius;
+    float area,perimeter;
+    radius=6;
+    cout << "Perimeter Of The Circle= "<< 2*(3.14*radius) <<" Inches"<< endl;
+    cout << "Area Of The Circle= "<< 3.14*radius*radius <<" Square Inches" << endl;
+    return 0;
+}
